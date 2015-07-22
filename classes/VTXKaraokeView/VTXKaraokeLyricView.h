7d44c929736f8d890a2613333065dc0f08723703
dphans <baophan94@icloud.com>
2015-07-22 18:11:53 +0700

    Update VTXKaraokeLyricView.h

--- classes/VTXKaraokeView/VTXKaraokeLyricView.h
+++ classes/VTXKaraokeView/VTXKaraokeLyricView.h
@@ -12,7 +12,10 @@
 @protocol VTXKaraokeLyricViewDelegate;
 
 @interface VTXKaraokeLyricView : UILabel
-@property (strong, nonatomic) UIColor *fillTextColor;
+@property (strong, nonatomic) UIColor *activeTextColor;
+@property (strong, nonatomic) UIColor *activeTextOutlineColor;
+@property (strong, nonatomic) UIColor *normalTextColor;
+@property (strong, nonatomic) UIColor *normalTextOutlineColor;
 @property (nonatomic) CGFloat duration;
 @property (nonatomic) NSDictionary *lyricSegment;
 @property (weak, nonatomic) id<VTXKaraokeLyricViewDelegate> delegate;
