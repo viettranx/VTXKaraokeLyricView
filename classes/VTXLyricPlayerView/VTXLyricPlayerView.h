b96d3a16ae04d62287b7c965726f9c8e6bf7c18e
Tran Xuan Viet <viettranx@gmail.com>
2015-07-23 09:58:09 +0700

    Create VTXLyricPlayerView class

---
+++ classes/VTXLyricPlayerView/VTXLyricPlayerView.h
@@ -0,0 +1,13 @@
+//
+//  VTXLyricPlayerView.h
+//  
+//
+//  Created by Tran Viet on 7/23/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VTXLyricPlayerView : UIView
+
+@end
---
+++ classes/VTXLyricPlayerView/VTXLyricPlayerView.m
@@ -0,0 +1,21 @@
+//
+//  VTXLyricPlayerView.m
+//  
+//
+//  Created by Tran Viet on 7/23/15.
+//
+//
+
+#import "VTXLyricPlayerView.h"
+
+@implementation VTXLyricPlayerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
