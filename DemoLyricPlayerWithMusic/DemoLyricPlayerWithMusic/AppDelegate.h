eaebfa1c0cc88540d63f9f2f30d6d54d0c740996
Tran Xuan Viet <viettranx@gmail.com>
2015-07-23 09:57:19 +0700

    Create DemoLyricWithMusic Project

---
+++ DemoLyricPlayerWithMusic/DemoLyricPlayerWithMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoLyricPlayerWithMusic
+//
+//  Created by Tran Viet on 7/23/15.
+//  Copyright (c) 2015 Viettranx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
